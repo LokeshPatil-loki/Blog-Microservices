625dee2719d59cc25618844003560689a56f6bf4
LokeshPatil-loki <lokesh1404@outlook.com>
2022-12-11 22:03:17 +0530

    Storing events in events store

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,19 @@ const PORT = 4005;
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post("/events",(req,res) => {
     const event = req.body;
+    events.push(event);
     axios.post("http://localhost:4000/events",event).catch(err => console.log(err.message));
     axios.post("http://localhost:4001/events",event).catch(err => console.log(err.message));
     axios.post("http://localhost:4002/events",event).catch(err => console.log(err.message));
     axios.post("http://localhost:4003/events",event).catch(err => console.log(err.message));
 
     res.send({status: "OK"})
-})
+});
+
+
 
 app.listen(PORT,()=>console.log("listening on " + PORT));
