68cc4c263637ebc5ecc69ccb30c0a7b59ca993d8
LokeshPatil-loki <lokesh1404@outlook.com>
2022-12-11 20:34:32 +0530

    Added status property to comments

--- query/index.js
+++ query/index.js
@@ -19,8 +19,8 @@ app.post("/events",(req,res) => {
         posts[id] = {id,title,comments:[]};
     }
     if(type === "CommentCreated"){
-        const {id, content, postId} = data;
-        posts[postId].comments.push({id, content});
+        const {id, content, postId, status} = data;
+        posts[postId].comments.push({id, content, status});
     }
     console.log(type, posts);
 });
